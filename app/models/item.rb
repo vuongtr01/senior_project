17ceea8cbd754070f5e87f8c281f10f06890b20a
vuongtr01 <vuontr01@luther.edu>
2023-11-13 16:51:03 -0600

    add search item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,32 @@
 class Item < ApplicationRecord
+  include PgSearch::Model
   belongs_to :closet
 
   PERMITTED_ATTRS = [
     :id, :name, :buy_date, :expr_date, :amount, :location, :image, :price, :closet_id,
   ]
+
+  pg_search_scope :search_by_name,
+  against: :name,
+  using: {
+    tsearch: { prefix: true, any_word: true },
+  }
+
+  scope :valid_search, lambda {
+    where.not(name: nil)
+  }
+
+  scope :search, ->(key, selected = [], closet_id = nil) do
+    return if key.blank?
+    search = valid_search.search_by_name(key)
+    if selected.present?
+      search = search.where.not(id: selected)
+    end
+    if closet_id.present?
+      search = search.where(closet_id: closet_id)
+    end
+    search
+  end
 
   def json_data
     self.as_json(only: [
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,80 @@ RSpec.describe Item, type: :model do
   context 'associations' do
     it { is_expected.to belong_to :closet }
   end
+
+  describe "#data_json" do
+    let(:closet) { FactoryBot.create(:closet) }
+    let(:item) { FactoryBot.create(:item, closet: closet) }
+    let(:expected_result) do
+      {
+        "id": item.id,
+        "name": item.name,
+        "location": item.location,
+        "buy_date": item.buy_date,
+        "expr_date": item.expr_date,
+        "amount": item.amount,
+        "image": item.image,
+        "price": item.price,
+        "closet_id": closet.id,
+      }
+    end
+
+    subject(:result) { item.json_data }
+
+    before do
+      expected_result
+    end
+    context 'get json data' do
+      it 'return json data' do
+        expect(result["id"]).to eq expected_result[:id]
+        expect(result["name"]).to eq expected_result[:name]
+      end
+    end
+  end
+
+  describe "scope search" do
+    let(:item1) { FactoryBot.create(:item, name: "Book") }
+    let(:item2) { FactoryBot.create(:item, name: "Blog") }
+    let(:selected) {[]}
+    let(:key) { "B" }
+    let(:closet_id) { nil }
+
+    before do
+      item1
+      item2
+    end
+
+    subject { Item.search(key, selected, closet_id) }
+
+    context "when selected params isn't present" do
+      it 'includes both item1 and item2' do
+        is_expected.to include item1
+        is_expected.to include item2
+      end
+    end
+
+    context "when selected params isn present" do
+      let(:selected) { [item1.id] }
+      it 'includes item2 only' do
+        is_expected.to include item2
+        is_expected.not_to include item1
+      end
+    end
+
+    context "search within closet" do
+      let(:closet) { FactoryBot.create(:closet) }
+      let(:item3) { FactoryBot.create(:item, closet: closet, name: "Banana") }
+      let(:closet_id) { closet.id }
+
+      before do
+        item3
+      end
+
+      it 'includes item2 only' do
+        is_expected.to include item3
+        is_expected.not_to include item1
+        is_expected.not_to include item2
+      end
+    end
+  end 
 end
