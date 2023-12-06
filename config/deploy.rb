46bd70a58698526fed1eae7d540c30d930046653
vuongtr01 <vuontr01@luther.edu>
2023-12-06 00:44:27 -0600

    add path to master.key

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,6 +42,7 @@ set :puma_systemctl_user, :system
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
+set :linked_files, %w{config/master.key}
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
