2761147c84b609871a91e26f95b48c554aae4f6a
vuongtr01 <vuontr01@luther.edu>
2023-12-06 01:21:22 -0600

    update deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,10 +7,9 @@ set :application,     'wolfpack'
 set :rbenv_type, :user 
 set :rbenv_ruby,      '3.2.2'
 # set :rbenv_ruby_dir,  '/home/ubuntu/.rbenv/versions/3.0.2'
-set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :rbenv_roles, :all # default value
+# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+# set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+# set :rbenv_roles, :all # default value
 
 # If using Digital Ocean's Ruby on Rails Marketplace framework, your username is 'rails'
 set :user,            'ubuntu'
