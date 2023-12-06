ed5a79eb2e572c74f26b0c4435fbac65195b2651
vuongtr01 <vuontr01@luther.edu>
2023-12-06 11:33:20 -0600

    add decrypt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Rails.application.configure do
   config.require_master_key = false
   config.read_encrypted_secrets = false
   config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
+  config.active_support.key_generator_hash_digest_class = OpenSSL::Digest::SHA1
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
