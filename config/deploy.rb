9a41465991cfc8833fc6574c24dda1c1f8796d40
vuongtr01 <vuontr01@luther.edu>
2023-12-06 11:05:18 -0600

    add namespace

--- config/deploy.rb
+++ config/deploy.rb
@@ -71,6 +71,16 @@ namespace :deploy do
     end
   end
 
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/master.key ]")
+          upload! 'config/master.key', "#{shared_path}/config/master.key"
+        end
+      end
+    end
+  end
+
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
